98465ece39b8cdfc99916c07c4e3af5bd3f6c0be
Alexander Balabanov <baal72alex@gmail.com>
2020-09-01 10:00:32 +0300

    Fix conditional rendering

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,34 +1,31 @@
-import React, {useLayoutEffect} from 'react';
+import React, {useEffect} from 'react';
 import UserInfo from './UserInfo';
 import {useDispatch, useSelector} from 'react-redux';
 import * as action from '../../redux/actions/actions'
 
 const  Users = () => {
-  const dispach = useDispatch();
-  useLayoutEffect(()=>{
-    dispach(action.setUsers());
-  },[dispach]);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    dispatch(action.setUsers());
+  },[dispatch]);
 
   const users = useSelector(state => state.users.users);
 
+  let content;
   if(!users.length){
-    return( <div className="row">
-      <h2>Server not available</h2>
-    </div>
-    )
+     content = <div className="row"><h2>Server not available</h2></div>;
+  }else{
+    content = users.map(user => {
+        return <UserInfo
+          id={user.id}
+          name={user.name}
+          username={user.username}
+          email={user.email}
+          key={user.id}
+        />
+      });
   }
-
-  return (
-    users.map(user => {
-      return <UserInfo
-        id={user.id}
-        name={user.name}
-        username={user.username}
-        email={user.email}
-        key={user.id}
-      />
-    })
-  )
+  return (content)
 }
 
 export default Users;
